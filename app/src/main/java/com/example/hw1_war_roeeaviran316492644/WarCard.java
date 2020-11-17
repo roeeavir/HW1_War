be26fd009cb3dea56474c003ecfb39d2d5c9405c
Roee <roeeavir@gmail.com>
2020-11-17 16:28:20 +0200

    Added code annotation to WarCard class

--- app/src/main/java/com/example/hw1_war_roeeaviran316492644/WarCard.java
+++ app/src/main/java/com/example/hw1_war_roeeaviran316492644/WarCard.java
@@ -1,9 +1,11 @@
 package com.example.hw1_war_roeeaviran316492644;
 
-public class WarCard{
+public class WarCard{// Class for implementing card ID and value
+    // Variables
     final private int card_ID;
     final private int card_Value;
 
+    // Constructor
     public WarCard(int card_ID, int card_Value) {
         this.card_ID = card_ID;
         this.card_Value = card_Value;
@@ -11,9 +13,9 @@ public class WarCard{
 
     public int getCard_ID() {
         return card_ID;
-    }
+    }// Get card ID
 
     public int getCard_Value() {
         return card_Value;
-    }
+    }// Get card value
 }
